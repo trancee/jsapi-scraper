e621290509ee9b99caa210b78b09aa230aad7c16
Philipp Grosswiler <philipp.grosswiler@gmail.com>
2023-03-25 21:27:43 +0100

    added Mediamarkt tests

--- shop/mediamarkt.go
+++ shop/mediamarkt.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 )
 
-var MediamarktRegex = regexp.MustCompile(` - |\s+20[12]\d|\s+[2345]G`)
+var MediamarktRegex = regexp.MustCompile(` - |\s+\(?20[12]\d\)?|\s+[2345]G|\s+((EE )?Enterprise Edition( CH)?)`)
 
 var MediamarktCleanFn = func(name string) string {
 	if loc := MediamarktRegex.FindStringSubmatchIndex(name); loc != nil {
@@ -25,7 +25,8 @@ var MediamarktCleanFn = func(name string) string {
 func XXX_mediamarkt(isDryRun bool) IShop {
 	const _name = "Mediamarkt"
 	// const _url = "https://www.mediamarkt.ch/de/category/_smartphone-680815.html?searchParams=&sort=price&view=PRODUCTGRID"
-	const _url = "https://www.mediamarkt.ch/de/category/_smartphone-680815.html?searchParams=%2FSearch.ff%3FfilterCategoriesROOT%3DHandy%2B%2526%2BNavigation%25C2%25A7MediaCHdec680760%26filterCategoriesROOT%252FHandy%2B%2526%2BNavigation%25C2%25A7MediaCHdec680760%3DSmartphone%25C2%25A7MediaCHdec680815%26filteravailability%3D1%26filterTyp%3D___Smartphone%26channel%3Dmmchde%26followSearch%3D9782%26disableTabbedCategory%3Dtrue%26navigation%3Dtrue&sort=price&view=PRODUCTGRID&page="
+	// const _url = "https://www.mediamarkt.ch/de/category/_smartphone-680815.html?searchParams=%2FSearch.ff%3FfilterCategoriesROOT%3DHandy%2B%2526%2BNavigation%25C2%25A7MediaCHdec680760%26filterCategoriesROOT%252FHandy%2B%2526%2BNavigation%25C2%25A7MediaCHdec680760%3DSmartphone%25C2%25A7MediaCHdec680815%26filteravailability%3D1%26filterTyp%3D___Smartphone%26channel%3Dmmchde%26followSearch%3D9782%26disableTabbedCategory%3Dtrue%26navigation%3Dtrue&sort=price&view=PRODUCTGRID&page=%d"
+	const _url = "https://www.mediamarkt.ch/de/category/_smartphone-680815.html?sort=price&searchParams=/Search.ff?filterTabbedCategory%3Donlineshop%26filterCategoriesROOT%3DHandy%2B%2526%2BNavigation%25C2%25A7MediaCHdec680760%26filterCategoriesROOT%252FHandy%2B%2526%2BNavigation%25C2%25A7MediaCHdec680760%3DSmartphone%25C2%25A7MediaCHdec680815%26filteravailability%3D1%26filterTyp%3D___Smartphone%26channel%3Dmmchde%26followSearch%3D9809%26disableTabbedCategory%3Dtrue%26navigation%3Dtrue&page="
 
 	const _debug = false
 	const _tests = false
@@ -52,97 +53,107 @@ func XXX_mediamarkt(isDryRun bool) IShop {
 	}
 	path += "/"
 
-	fn := "shop/mediamarkt.html"
+	for p := 1; p <= 5; p++ {
+		fn := fmt.Sprintf("shop/mediamarkt.%d.html", p)
 
-	if isDryRun {
-		if body, err := os.ReadFile(path + fn); err != nil {
-			panic(err)
+		if isDryRun {
+			if body, err := os.ReadFile(path + fn); err != nil {
+				panic(err)
+			} else {
+				_body = body
+			}
 		} else {
-			_body = body
-		}
-	} else {
-		resp, err := http.Get(_url)
-		if err != nil {
-			panic(err)
-		}
-		defer resp.Body.Close()
+			resp, err := http.Get(fmt.Sprintf("%s%d", _url, p))
+			if err != nil {
+				panic(err)
+			}
+			defer resp.Body.Close()
 
-		if body, err := io.ReadAll(resp.Body); err != nil {
-			panic(err)
+			if body, err := io.ReadAll(resp.Body); err != nil {
+				panic(err)
+			} else {
+				_body = body
+			}
+
+			os.WriteFile(path+fn, _body, 0664)
+		}
+		// fmt.Println(string(_body))
+
+		doc := parse(string(_body))
+
+		productList := traverse(doc, "ul", "class", "products-grid")
+		// fmt.Println(productList)
+
+		for item := productList.FirstChild.NextSibling; item != nil; item = item.NextSibling.NextSibling {
+			// fmt.Println(item)
+
+			_product := _Response{}
+
+			baseInfo := traverse(item, "div", "class", "base-info")
+			// fmt.Println(baseInfo)
+
+			productKey, _ := attr(baseInfo.Attr, "data-reco-pid")
+			// fmt.Println(productKey)
+			productId := productKey[2:]
+			if _debug {
+				fmt.Println(productId)
+			}
+			_product.code = productId
+
+			imageTitleLink := traverse(baseInfo, "a", "class", "product-link")
+			// fmt.Println(imageTitleLink)
+
+			link, _ := attr(imageTitleLink.Attr, "href")
+			if _debug {
+				fmt.Println(link)
+			}
+			_product.link = link
+
+			title, _ := text(imageTitleLink)
+			// title = strings.TrimSpace(strings.Split(strings.ReplaceAll(strings.ReplaceAll(title, " - Smartphone", ""), " \"", "\""), "(")[0])
+			if _debug {
+				fmt.Println(title)
+			}
+			_product.title = title
+
+			if Skip(title) {
+				continue
+			}
+
+			model := MediamarktCleanFn(_product.title)
+			if _debug {
+				fmt.Println(model)
+			}
+			_product.model = model
+
+			currentPrice := traverse(baseInfo, "div", "class", "price")
+			// fmt.Println(currentPrice)
+
+			price, _ := text(currentPrice)
+			if _debug {
+				fmt.Println(price)
+			}
+
+			if _price, err := strconv.ParseFloat(strings.ReplaceAll(price, ".-", ".00"), 32); err != nil {
+				panic(err)
+			} else {
+				_product.oldPrice = float32(_price)
+			}
+
+			if _debug {
+				fmt.Println()
+			}
+
+			_result = append(_result, _product)
+		}
+
+		if results := traverse(doc, "li", "class", "pagination-next"); results != nil {
+			if _, exists := attr(results.Attr, "data-value"); !exists {
+				break
+			}
 		} else {
-			_body = body
+			break
 		}
-
-		os.WriteFile(path+fn, _body, 0664)
-	}
-	// fmt.Println(string(_body))
-
-	doc := parse(string(_body))
-
-	productList := traverse(doc, "ul", "class", "products-grid")
-	// fmt.Println(productList)
-
-	for item := productList.FirstChild.NextSibling; item != nil; item = item.NextSibling.NextSibling {
-		// fmt.Println(item)
-
-		_product := _Response{}
-
-		baseInfo := traverse(item, "div", "class", "base-info")
-		// fmt.Println(baseInfo)
-
-		productKey, _ := attr(baseInfo.Attr, "data-reco-pid")
-		// fmt.Println(productKey)
-		productId := productKey[2:]
-		if _debug {
-			fmt.Println(productId)
-		}
-		_product.code = productId
-
-		imageTitleLink := traverse(baseInfo, "a", "class", "product-link")
-		// fmt.Println(imageTitleLink)
-
-		link, _ := attr(imageTitleLink.Attr, "href")
-		if _debug {
-			fmt.Println(link)
-		}
-		_product.link = link
-
-		title, _ := text(imageTitleLink)
-		// title = strings.TrimSpace(strings.Split(strings.ReplaceAll(strings.ReplaceAll(title, " - Smartphone", ""), " \"", "\""), "(")[0])
-		if _debug {
-			fmt.Println(title)
-		}
-		_product.title = title
-
-		if Skip(title) {
-			continue
-		}
-
-		model := MediamarktCleanFn(_product.title)
-		if _debug {
-			fmt.Println(model)
-		}
-		_product.model = model
-
-		currentPrice := traverse(baseInfo, "div", "class", "price")
-		// fmt.Println(currentPrice)
-
-		price, _ := text(currentPrice)
-		if _debug {
-			fmt.Println(price)
-		}
-
-		if _price, err := strconv.ParseFloat(strings.ReplaceAll(price, ".-", ".00"), 32); err != nil {
-			panic(err)
-		} else {
-			_product.oldPrice = float32(_price)
-		}
-
-		if _debug {
-			fmt.Println()
-		}
-
-		_result = append(_result, _product)
 	}
 
 	_parseFn := func(s IShop) *[]*Product {
